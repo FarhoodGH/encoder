7f4c4c2148fadaea2459ee83432fa00db3821711
Farhood Ghanbari <farhoodgh83@gmail.com>
2020-12-15 10:32:23 +0330

    Solved First and Second TODO
    1 - check if enter wrong word
    2 - customize encdoe (with rot13)

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 Coded By [Farhood Ghanbari](https://github.com/farhoodgh) & [Ali Javadi](https://github.com/javadimoghadam)
 
 ## Requirement
-> Python Version -> 3 or Eailer 
-> You need base64 & Colorama For Run This File
+> You need Python Version 3 or Eailer , base64 & Colorama For Run This File
 
 ## How to use 
 ```bash
@@ -12,12 +11,14 @@ step 2 -> cd encoder/
 step 3 -> python3 encoder.py
 ```
 
-# License -> [MIT](https://opensource.org/licenses/MIT)
 
 
-# TODO :|
+# TODO : 
 ```
-1- check if user enter wrong word 
-2 - customize encode
+1- check if user enter wrong word [Solved]
+2 - customize encode [Solved]
 3- license key 
 ```
+
+#### License -> [MIT](https://opensource.org/licenses/MIT)
+
--- encoder.py
+++ encoder.py
@@ -2,25 +2,30 @@ from os import system
 import base64
 from colorama import Fore
 
+#custom encode
+def rot13(phrase):
+   abc = "abcdefghijklmnopqrstuvwxyz"
+   out_phrase = ""
+   for char in phrase:
+       out_phrase += abc[(abc.find(char)+13)%26]
+   return out_phrase
+
 # encode
-def encode(data):
+def encode(data:str,originaldata:str):
+    data.lower()
     try:
         encodedbyte = base64.b64encode(data.encode("utf-8"))
-        print(Fore.GREEN + "Encoded Successfully :) \n " + "Data : " + data + "\n Encoded version : " + str(encodedbyte,"utf-8")  +"\n" + Fore.RESET)
+        print(Fore.GREEN + "Encoded Successfully :) \n " + "Data : " + originaldata + "\n Encoded version : " + str(encodedbyte,"utf-8")  +"\n" + Fore.RESET)
     except Exception as e:
         print(Fore.RED + "Encoded Failed :( \n Error :" + e + Fore.RESET)
-
+        
 # decode
-def decode(data):
+def decode(data:str):
     try:
         decodedbyte = base64.b64decode(data)
-        print(Fore.GREEN + "Decode Successfully :) \n " + "Data : " + data + "\n Decoded version : " + str(decodedbyte,"utf-8")  +"\n" + Fore.RESET)
+        print(Fore.GREEN + "Decode Successfully :) \n " + "Data : " + data + "\n Decoded version : " + rot13(str(decodedbyte,"utf-8")).replace("z"," ")  +"\n" + Fore.RESET)
     except Exception as e:
         print(Fore.RED + "Decode Failed :( \n Error :" + e + Fore.RESET)
-
-#Switch for validate
-
-
 
 # Start a Software  
 if __name__ == "__main__":
@@ -41,13 +46,18 @@ if __name__ == "__main__":
 
 
 while True:
-    d = (input(Fore.WHITE + "Please enter your Method( [D]ecode ,  [E]ncode , [e]xit ): " +Fore.RESET))
-
+    d = (input(Fore.WHITE + "Please enter your Method( [D]ecode ,  [E]ncode , [e]xit ) -> " +Fore.RESET))
     if d is "e": 
+        system("cls")
+        print(Fore.GREEN + "Exit Successfully ! " + Fore.RESET)
         exit()
-    if d is "D":
-        Dcode = (input(Fore.BLUE  +  "Please enter your code: "  + Fore.RESET))
+    elif d is "D":
+        Dcode = (input(Fore.BLUE  +  "Please enter your code -> "  + Fore.RESET))
         decode(Dcode)
-    if d is "E":
-        Ecode = (input(Fore.BLUE + "Please enter your text:  " + Fore.RESET))
-        encode(Ecode)
+        
+    elif d is "E":
+        Ecode = (input(Fore.BLUE + "Please enter your text -> " + Fore.RESET))
+        custom = rot13(Ecode)
+        encode(custom,Ecode)
+    else:
+        print(Fore.RED + "Wrong argument !" +Fore.RESET) 
